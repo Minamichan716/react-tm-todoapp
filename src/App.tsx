10143f7e622794bd195c503e11642eb864856598
Minamichan716 <ampm.716@gmail.com>
2023-12-28 12:30:43 +0900

    12/27

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,13 @@ function App() {
 
   // 空の配列に何が入るかを指定する(Todoで宣言した3つの配列を持つ型)
   const [todos , setTodos] = useState<Todo[]>([]);
-
+  // 完了ボタン 
+  const [completetodos, setCompletetodos] = useState<Todo[]>([
+    
+  ]);
+  // 未完了
+  // const [incompleteTodos, setIncompleteTodos] = useState<Todo[]>([]);
+ 
 
   // 型を指定しておく
   type Todo = {
@@ -25,19 +31,17 @@ function App() {
   // 入力したTodo
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputText(event.target.value)
-    console.log(event.target.value);
-    
+   
   }
 
   // 完了予定日の取得
   const targetDate = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputDate(event.target.value)
-    console.log(event.target.value);
-    
+    // console.log(event.target.value);   
   }
 
   // リスト追加の処理
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const onSubmitTodoAdd = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
 
     // 新しいTodo作成
@@ -72,19 +76,39 @@ function App() {
 
   // 削除ボタン
   const onClickDelete = (index:any) => {
+
     const newTodos = [...todos];
     newTodos.splice(index, 1);
     setTodos(newTodos);
   }
+
+  const completeDelete = (index:any) => {
+    const newCompleteTodos = [...completetodos];
+    newCompleteTodos.splice(index,1);
+    setCompletetodos(newCompleteTodos)
+  }
+
+
+
+  // 完了ボタン
+  const onClickComplete = (index:any) => {
+   
+    const newIncompleteTodos = [...todos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompleteTodos = [...completetodos,todos[index]];
+    setTodos(newIncompleteTodos);
+    setCompletetodos(newCompleteTodos);
+   }
 
 
 
   return (
     <div className="App">
       <h1>Todo List</h1>
-        <form onSubmit={(event)=> handleSubmit(event)}>
-        <input onChange={(event) =>handleChange(event)}type="text" className='inputText'/>
-        <input onChange={(event) =>targetDate(event)} name="date" type="date" className='inputDate' />
+        <form onSubmit={(event)=> onSubmitTodoAdd(event)}>
+        <input onChange={(event) =>handleChange(event)}type="text" value={inputText}className='inputText'/>
+        <input onChange={(event) =>targetDate(event)} name="date" type="date" value={inputDate}className='inputDate' />
         <input onChange={() => {}} type="submit" value="追加" className="submitButton" />
         </form>
       <div className='container'>
@@ -109,7 +133,7 @@ function App() {
             {todos.map((todo,index) => (
               <li key={todo.id} className='inProgress' ><span className='tobeDone'>{todo.targetDate}</span><input onChange={(event) =>handleEdit(todo.id,event.target.value)}type="text" value={todo.inputText} className='inputText'/>
                           <div className='buttonWrap'>
-            <button onClick={()=> {}}className='completeButton'>完了</button>
+            <button onClick={()=> onClickComplete(index)}className='completeButton'>完了</button>
             <button onClick={()=>onClickDelete(index)} className='deleteButton'>削除</button>
 
             </div>
@@ -121,18 +145,29 @@ function App() {
         <div className='doneList'>
         <h2>完了リスト</h2>
           <ul>
-            <li className='done'>ご飯食べる
+            {/* <li className='done'>ご飯食べる
             <div className='buttonWrap'>
             <button className='backButton'>戻す</button>
             <button className='deleteButton'>削除</button>
             </div>
-            </li>
-            <li className='done'>掃除選択大掃除
+            </li> */}
+            {/* <li className='done'>掃除選択大掃除
             <div className='buttonWrap'>
             <button className='backButton'>戻す</button>
             <button className='deleteButton'>削除</button>
             </div>
-            </li>
+            </li> */}
+            {completetodos.map((todo,index) => (
+              <li key={todo.id} className='done' ><span className='tobeDone'>{todo.targetDate}</span><input onChange={(event) =>handleEdit(todo.id,event.target.value)}type="text" value={todo.inputText} className='inputText'/>
+                          <div className='buttonWrap'>
+ 
+            <button onClick={()=> onClickComplete(index)} className='backButton'>戻す</button>
+            <button onClick={()=>completeDelete(index)}  className='deleteButton'>削除</button>
+
+            </div>
+              </li>
+            ))}
+
           </ul>
         </div>
 
